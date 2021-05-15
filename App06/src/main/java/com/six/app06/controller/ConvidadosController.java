44a564654887459a88b8216779dfa4c04438529d
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-05-15 02:33:07 -0300

    adding app06

--- App06/src/main/java/com/six/app06/controller/ConvidadosController.java
+++ App06/src/main/java/com/six/app06/controller/ConvidadosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,7 +21,7 @@ public class ConvidadosController {
     @GetMapping("/")
     public ModelAndView home(){
 
-        ModelAndView view = new ModelAndView("convidados/index");
+        ModelAndView view = new ModelAndView("convidados/index2");
 
         view.addObject("convidados", repository.findAll());
         view.addObject("objConvidadoForm", new ConvidadosModels());
@@ -30,6 +31,25 @@ public class ConvidadosController {
     @PostMapping("/")
     String redirect(ConvidadosModels entity){
         repository.save(entity);
+        return "redirect:/";
+    }
+
+    @GetMapping("/convidados/{id}")
+    public ModelAndView editar(@PathVariable Long id){
+
+        ConvidadosModels objId = repository.findById(id).get();
+        ModelAndView view = new ModelAndView("convidados/index3");
+
+        view.addObject("objId", objId);
+
+        return view;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable Long id){
+
+        repository.deleteById(id);
+
         return "redirect:/";
     }
 }
