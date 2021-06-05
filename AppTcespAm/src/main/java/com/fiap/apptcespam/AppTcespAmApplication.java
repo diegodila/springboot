3b7b9b1037afc00410a0cb33605e0f7777ebb7ad
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-06-05 11:10:36 -0300

    removing it-service, java, produtos-mvc on verificar

---
+++ AppTcespAm/src/main/java/com/fiap/apptcespam/AppTcespAmApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.apptcespam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTcespAmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTcespAmApplication.class, args);
+	}
+
+}
---
+++ AppTcespAm/src/test/java/com/fiap/apptcespam/AppTcespAmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.apptcespam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTcespAmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
